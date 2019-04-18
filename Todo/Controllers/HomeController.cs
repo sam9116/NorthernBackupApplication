67e373b2a4cd709c41fa663832c50c22b63bf2e9
sam bao <sam.b5687@gmail.com>
2019-04-18 03:10:30 -0600

    Initial Upload

---
+++ Todo/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int limit = 10)
+        {
+            List<Post> PostList = Postgetter.GetPosts();
+            return View(PostList.Take(limit).ToList());
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Todo/Models/Todo.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Summary description for Todo
+/// </summary>
+
+namespace Todo.Models
+{
+    public class Post
+    {
+        public int userId;
+        public int id;
+        public string title;
+        public string body;
+
+        public Post()
+        {
+            title = "";
+            body = "";
+
+        }
+        public Post(int userIdinput, int idinput, string titleinput, string bodyinput)
+        {
+            userId = userIdinput;
+            id = idinput;
+            title = titleinput;
+            body = bodyinput;
+        }        
+    }
+    public static class Postgetter
+    {
+        public static List<Post> GetPosts()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://jsonplaceholder.typicode.com/posts");
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var postlist = JsonConvert.DeserializeObject<List<Post>>(reader.ReadToEnd());
+                return postlist;
+            }
+        }
+    }
+
+}
+
